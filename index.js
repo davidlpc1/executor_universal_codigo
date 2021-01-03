1ea4ad888ece96738e0b6d378c82382fcadce366
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-03 17:10:29 -0300

    Everthing ready,we compile js java and python

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const { exec } = require('shelljs');
+const readline = require('readline-sync');
+
 console.log("Executor universal de código")
+console.log("Você precisará de NodeJs,Java e Python instalados")
 
-const requestJS ="console.log('Resultado:',3 + 4)";
+const willContinue = readline.question('Deseja continuar (yes/no): ')
+if(willContinue.match(/no/gi) !== null || willContinue.match(/nao/gi)){
+    process.exit(0);
+}
+
+const separeOutput = () => console.log('-----------------------------------------------------------------------------')
+const showResultOfCoding = (language,code) => {
+    console.log(`Código ${language} stdout: ${code.stdout}`)
+    separeOutput()
+    console.log(`Código ${language} stderr: ${code.stderr}`)
+    separeOutput()
+}
+
+const requestJS = readline.question('Insira o codigo JavaScript: ')
 const codigoJS = exec(`node -p "${requestJS}"`,{ silent: true})
+showResultOfCoding('JavaScript',codigoJS)
 
-const requestPython = "print('Hello world')"
+const requestPython = readline.question('Insira o codigo Python: ')
 const codigoPython = exec(`echo ${requestPython} > arquivo.py && python arquivo.py`,{ silent: true});
+showResultOfCoding('Python',codigoPython)
 
-const { log } = console;
-
-log(`Código Python stdout: ${codigoPython.stdout}`)
-log('-----------------------------------------------------------------------------')
-log(`Código Python stderr: ${codigoPython.stderr}`)
-log('-----------------------------------------------------------------------------')
-log(`Código JavaScript stdout: ${codigoJS.stdout}`)
-log('-----------------------------------------------------------------------------')
-log(`Código JavaScript stderr: ${codigoJS.stderr}`)
-log('-----------------------------------------------------------------------------')
+//class Main {public static void main(String[] args) {System.out.println("Hello World"); } };
+const requestJava = readline.question('Insira o codigo Java: ')
+const codigoJava = exec(`echo ${requestJava} > Main.java && javac Main.java && java Main`,{ silent: true})
+showResultOfCoding('Java',codigoJava)
